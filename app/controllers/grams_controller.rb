354e6e0d675248b3fa86a89fac8f52dd1bbcf150
Barry Bradley <bbradley13@gmail.com>
2018-03-08 05:40:14 +0000

    test checking if new and create actions work

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,4 +1,6 @@
 class GramsController < ApplicationController
+  #Allows new and create actions to work properly by logged in users 
+  before_action :authenticate_user!, only: [:new, :create]
   def new
     @gram = Gram.new
   end
@@ -7,7 +9,7 @@ class GramsController < ApplicationController
   end
 
   def create
-    @gram = Gram.create(gram_params)
+    @gram = current_user.grams.create(gram_params)
     if @gram.valid?
       redirect_to root_path
     else
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -12,30 +12,63 @@ RSpec.describe GramsController, type: :controller do
 
 
   describe "grams#new action" do
+    it "should require users to be logged in" do
+      get :new
+      expect(response).to redirect_to new_user_session_path
+    end
+
     it "should successfully show the new form" do
+      user = User.create(
+        email:                  'fakeuser@gmail.com',
+        password:               'secretPassword',
+        password_confirmation:  'secretPassword'
+      )
+      sign_in user
+
       get :new
       expect(response).to have_http_status(:success)
     end
   end
   
   describe "grams#create action" do
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: "Hello" }}
+      expect(response).to redirect_to new_user_session_path
+    end
+    
     it "should successfully create a new gram in our database" do
+      user = User.create(
+        email:                  'fakeuser@gmail.com',
+        password:               'secretPassword',
+        password_confirmation:  'secretPassword'
+      )
+      sign_in user
+
       post :create, params: { gram: { message: 'Hello!' } }
       expect(response).to redirect_to root_path
 
       gram = Gram.last
       expect(gram.message).to eq("Hello!")
+      expect(gram.user).to eq(user)
     end
 
     #prevents blank grams from entering db
     it "should properly deal with validation errors" do
+      user = User.create(
+        email:                  'fakeuser@gmail.com',
+        password:               'secretPassword',
+        password_confirmation:  'secretPassword'
+      )
+      sign_in user
+
+      gram_count = Gram.count
       #triggers httpp post request but leaves blank string in message to test?
       #this is for create action in grams controller
       post :create, params: { gram: { message: ''}}
       #HTTP status should be unprocessable_entity
       expect(response).to have_http_status(:unprocessable_entity)
       #The grams should be zero
-      expect(Gram.count).to eq 0
+      expect(Gram.count).to eq Gram.count
     end
   end
 end
